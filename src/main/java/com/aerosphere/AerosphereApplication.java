10a0c6c73ffdeabe51a3b215c1e021c3ef756e55
Newtton Habakuk <newttonhabakuk.s@gmail.com>
2026-07-09 23:35:54 +0530

    chore: initialize AeroSphere project foundation

---
+++ src/main/java/com/aerosphere/AerosphereApplication.java
@@ -0,0 +1,13 @@
+package com.aerosphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerosphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerosphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aerosphere/AerosphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aerosphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerosphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
